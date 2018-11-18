6f663daf40d19984fb57a1f07a10183f536d3cf2
Matt McDermott <matt.mcdermott@idfive.com>
2018-11-18 14:26:19 -0500

    more

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 console.log('Hello World!');
+
 console.log('Yeah, boy!');
+
 var numberOfDogs = 10;
+
 var numberOfKittens = 5;
+
 console.log (numberOfDogs);
+
 var numberOfAnimals = numberOfDogs + numberOfKittens;
-console.log(numberOfAnimals)
+
+console.log(numberOfAnimals);
+
 var dogName = 'Pancho';
+
 console.log("I'd like to buy some crack");
+
 var MurderTown = dogName + numberOfAnimals;
-console.log(MurderTown)
+
+console.log(MurderTown);
