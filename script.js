04906687ac7667574c5d2cf41c191c045afc8301
Matt McDermott <matt.mcdermott@idfive.com>
2018-11-18 14:04:28 -0500

    More changes

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('Hello World!');
+console.log('Yeah, boy!');
+document.write('Hello World!')
+var numberOfDogs = 10;
+console.log (numberOfDogs);
+console.log (numberOfDogs);
+console.log (numberOfDogs);
+console.log (numberOfDogs);
+console.log (numberOfDogs);
+console.log (numberOfDogs);
