f71ba8be0f906310e195452385aeb50485009d00
Matt McDermott <matt.mcdermott@idfive.com>
2018-11-18 14:18:24 -0500

    Yep

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 console.log('Hello World!');
 console.log('Yeah, boy!');
-document.write('Hello World!')
 var numberOfDogs = 10;
+var numberOfKittens = 5;
 console.log (numberOfDogs);
-console.log (numberOfDogs);
-console.log (numberOfDogs);
-console.log (numberOfDogs);
-console.log (numberOfDogs);
-console.log (numberOfDogs);
+var numberOfAnimals = numberOfDogs + numberOfKittens;
+console.log(numberOfAnimals)
+var dogName = 'Pancho';
+console.log("I'd like to buy some crack");
+var MurderTown = dogName + numberOfAnimals;
+console.log(MurderTown)
